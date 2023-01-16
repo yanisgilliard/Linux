337a9e27baeefa1afce824c5a16b6f6d342f6a3f
yanisgilliard <114937860+yanisgilliard@users.noreply.github.com>
2023-01-16 16:18:46 +0100

    Create script.sql

---
+++ TP5/script.sql
@@ -0,0 +1,4 @@
+CREATE USER 'nextcloud'@'10.105.1.11' IDENTIFIED BY 'test';
+CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'10.105.1.11';
+FLUSH PRIVILEGES;
